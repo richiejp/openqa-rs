e8c4c326bc0735bd395303d84620b0424b0c0e1b
Richard Palethorpe <rpalethorpe@suse.com>
2018-06-15 11:18:21 +0200

    plan

---
+++ src/main.rs
@@ -0,0 +1,7 @@
+fn main() {
+    // Get list of test cases
+    // For each test case
+    // 	If it has a publish HDD_1 generate a publish pflash vars
+    //  If it has a HDD_1 and a START_AFTER_TEST, generate a UEFI_PFLASH_VARS
+    //  present changes for sanity check
+}
